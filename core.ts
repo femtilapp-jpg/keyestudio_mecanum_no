d7b011ea59bbac32e10a7d23b709bb8f3599801d
femtilapp-jpg <femtilapp@gmail.com>
2026-01-03 01:23:13 +0100

    Oppdater pxt.json, main.blocks, core.ts

---
+++ core.ts
@@ -0,0 +1,7 @@
+//% color=190 weight=100 block="Keyestudio PRO"
+namespace keyestudioPro {
+    //% block="test blokk"
+    export function testBlokk(): void {
+        basic.showIcon(IconNames.Heart)
+    }
+}
