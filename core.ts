d4427c8175464ce5691948ba602d48aa195bfce3
femtilapp-jpg <femtilapp@gmail.com>
2026-01-03 03:00:39 +0100

    Update core.ts

--- core.ts
+++ core.ts
@@ -1,4 +1,4 @@
 //% color=102 icon="\uf0d1" weight=100 block="Mecanum 4WD"
-//% groups=['Start','Kjøring','Sensorer','Bluetooth','Lys','Autopilot','Avansert']
+//% groups=['Start','Kjøring','Sensorer','Bluetooth','Lys','Autopilot','Avansert','IR']
 namespace keyestudioPro {
 }
